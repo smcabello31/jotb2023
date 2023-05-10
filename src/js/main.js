dc39fd11808a6b44acbd6942fecce4287cfff7dd
Samuel Moron <samuel@novoshore.com>
2023-05-10 18:32:27 +0200

    add form page and functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 /* Aside & Navbar: dropdowns */
 
-Array.from(document.getElementsByClassName('dropdown')).forEach(elA => {
-  elA.addEventListener('click', e => {
+Array.from(document.getElementsByClassName('dropdown')).forEach((elA) => {
+  elA.addEventListener('click', (e) => {
     if (e.currentTarget.classList.contains('navbar-item')) {
       e.currentTarget.classList.toggle('active')
     } else {
@@ -16,56 +16,97 @@ Array.from(document.getElementsByClassName('dropdown')).forEach(elA => {
 
 /* Aside Mobile toggle */
 
-Array.from(document.getElementsByClassName('mobile-aside-button')).forEach(el => {
-  el.addEventListener('click', e => {
-    const dropdownIcon = e.currentTarget
+Array.from(document.getElementsByClassName('mobile-aside-button')).forEach(
+  (el) => {
+    el.addEventListener('click', (e) => {
+      const dropdownIcon = e.currentTarget
         .getElementsByClassName('icon')[0]
         .getElementsByClassName('mdi')[0]
 
-    document.documentElement.classList.toggle('aside-mobile-expanded')
-    dropdownIcon.classList.toggle('mdi-forwardburger')
-    dropdownIcon.classList.toggle('mdi-backburger')
-  })
-})
+      document.documentElement.classList.toggle('aside-mobile-expanded')
+      dropdownIcon.classList.toggle('mdi-forwardburger')
+      dropdownIcon.classList.toggle('mdi-backburger')
+    })
+  }
+)
 
 /* NavBar menu mobile toggle */
 
-Array.from(document.getElementsByClassName('--jb-navbar-menu-toggle')).forEach(el => {
-  el.addEventListener('click', e => {
-    const dropdownIcon = e.currentTarget
+Array.from(document.getElementsByClassName('--jb-navbar-menu-toggle')).forEach(
+  (el) => {
+    el.addEventListener('click', (e) => {
+      const dropdownIcon = e.currentTarget
         .getElementsByClassName('icon')[0]
         .getElementsByClassName('mdi')[0]
 
-    document.getElementById(e.currentTarget.getAttribute('data-target')).classList.toggle('active')
-    dropdownIcon.classList.toggle('mdi-dots-vertical')
-    dropdownIcon.classList.toggle('mdi-close')
-  })
-})
+      document
+        .getElementById(e.currentTarget.getAttribute('data-target'))
+        .classList.toggle('active')
+      dropdownIcon.classList.toggle('mdi-dots-vertical')
+      dropdownIcon.classList.toggle('mdi-close')
+    })
+  }
+)
 
 /* Modal: open */
 
-Array.from(document.getElementsByClassName('--jb-modal')).forEach(el => {
-  el.addEventListener('click', e => {
+Array.from(document.getElementsByClassName('--jb-modal')).forEach((el) => {
+  el.addEventListener('click', (e) => {
     const modalTarget = e.currentTarget.getAttribute('data-target')
 
     document.getElementById(modalTarget).classList.add('active')
     document.documentElement.classList.add('clipped')
   })
-});
+})
 
 /* Modal: close */
 
-Array.from(document.getElementsByClassName('--jb-modal-close')).forEach(el => {
-  el.addEventListener('click', e => {
-    e.currentTarget.closest('.modal').classList.remove('active')
-    document.documentElement.classList.remove('is-clipped')
-  })
-})
+Array.from(document.getElementsByClassName('--jb-modal-close')).forEach(
+  (el) => {
+    el.addEventListener('click', (e) => {
+      e.currentTarget.closest('.modal').classList.remove('active')
+      document.documentElement.classList.remove('is-clipped')
+    })
+  }
+)
 
 /* Notification dismiss */
 
-Array.from(document.getElementsByClassName('--jb-notification-dismiss')).forEach(el => {
-  el.addEventListener('click', e => {
+Array.from(
+  document.getElementsByClassName('--jb-notification-dismiss')
+).forEach((el) => {
+  el.addEventListener('click', (e) => {
     e.currentTarget.closest('.notification').classList.add('hidden')
   })
 })
+
+const submitButton = document.querySelector('#submitButton')
+submitButton.addEventListener('click', sendForm)
+
+const sendForm = () => {
+  const age = document.getElementById('age')
+  const socialClass = document.querySelector('#socialClass').value
+  const hasChronicDisease = document.querySelector(
+    '[name="hasChronicDisease"]'
+  ).value
+  const isSocialSecurityUser = document.getElementById(
+    '[name="isSocialSecurityUser"]'
+  ).value
+
+  const predictionData = {
+    age: age,
+    social_class: socialClass,
+    public_user: isSocialSecurityUser === 'on' ? 1 : 0,
+    long_term_illnes: hasChronicDisease === 'on' ? 1 : 0,
+  }
+
+  const url = 'localhost:5000/satisfaction'
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: predictionData,
+  }
+}
